79f590d589b67212e3751227c2a6039b04f9bc6e
DoQuanghealing <doduongquang8686@gmail.com>
2026-02-12 19:11:03 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-  },
   plugins: [react()],
   resolve: {
     alias: {
-      // Vì không có /src, chúng ta trỏ @ trực tiếp vào thư mục gốc
-      '@': path.resolve(__dirname, '.'),
+      '@': path.resolve(__dirname, '.'), // Trỏ @ trực tiếp vào root
     },
   },
   build: {
     outDir: 'dist',
-    // Đảm bảo Vite tìm đúng file entry chính tại thư mục gốc
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'index.html'),
